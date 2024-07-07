21e99e9228fdc7ba3bb54561c8ee14fd9188acdd
Artem Amaev <artemamaev2@gmail.com>
2024-07-07 13:25:43 +0300

    Initial Commit

---
+++ RickAndMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyApp
+//
+//  Created by Артём Амаев on 7.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Артём Амаев on 7.07.24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
